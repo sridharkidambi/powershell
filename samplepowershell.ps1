ce0247c2c88b422389e53ac270d0d1335ff1cc3c
Sridhar Kidambi <sridharkidambi@srinivasas-mbp.attlocal.net>
2017-07-05 23:27:25 -0500

    powershell filter

--- samplepowershell.ps1
+++ samplepowershell.ps1
@@ -1,8 +1,19 @@
 $a=1+2
 # get-member cmdlets is an object
 # $a |get-member
-$a.ToDecimal(4,2)
+# $a.ToDecimal(4,2)
 $files=dir
-$files[0]
-echo "iam sk" \n
-$files[1] | Get-Member
+$files[0].Name
+echo "iam sk" 
+
+# $files[1] | Get-Member
+$files |Sort-Object -Property CreationTime
+$files |Sort-Object -Descending Name
+$files |sort -Property Name
+
+$files | Where-Object {$_.Length -gt 4}
+
+foreach($file  in $files){
+   $file.Name
+}
+1..100 |foreach{"*"*$_}
